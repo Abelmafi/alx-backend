7d55731303c504a940026d2c7f9452607a815556
abel mesfin <abelmesfin555@gmail.com>
2023-04-25 21:36:34 +0300

    commited

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-"""...."""
-from flask import Flask
+'''Task 0: Basic Flask app
+'''
+
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
 class Config:
-    """Config class"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    '''Config class'''
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-app.config.from_object('Config')
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
 babel = Babel(app)
 
+
 @app.route('/')
 def index():
-    """..."""
-    render_template("1-index.html")
+    '''default route'''
+    return render_template("1-index.html",)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
