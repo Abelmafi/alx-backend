9f2719c108e6d473a00530cc8843f117fa19edd5
abel mesfin <abelmesfin555@gmail.com>
2023-05-25 21:02:38 +0300

    Writing the job creation function

---
+++ 0x03-queuing_system_in_js/8-jobs.js
@@ -0,0 +1,47 @@
+const kue = require('kue');
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job
+      .on('enqueue', () => {
+        console.log(`Notification job created: ${job.id}`);
+      })
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('failed', (error) => {
+        console.log(`Notification job ${job.id} failed: ${error}`);
+      })
+      .on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+
+    job.save();
+  });
+}
+
+const queue = kue.createQueue();
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
+
+createPushNotificationsJobs(jobs, queue);
+
